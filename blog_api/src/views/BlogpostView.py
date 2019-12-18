d9b094a62cf90b697e11e84fe58411ac91d4e220
dzehirov <dvz2@njit.edu>
2019-12-18 18:32:55 -0500

    Task: Updated code in BlogpostView.py again

--- blog_api/src/views/BlogpostView.py
+++ blog_api/src/views/BlogpostView.py
@@ -69,3 +69,45 @@ def get_all():
 #####################
 # existing code remain #
 ######################
+
+
+#/src/views/BlogpostView.py
+# app initiliazation
+#####################
+# existing code remain #
+######################
+
+
+@blogpost_api.route('/', methods=['POST'])
+@Auth.auth_required
+def create():
+  """
+  Create Blogpost Function
+  """
+  # app initiliazation
+  #####################
+  # existing code remain #
+  ######################
+  return custom_response(data, 201)
+
+  # app initiliazation
+  #####################
+  # existing code remain #
+  ######################
+
+@blogpost_api.route('/<int:blogpost_id>', methods=['GET'])
+def get_one(blogpost_id):
+  """
+  Get A Blogpost
+  """
+  post = BlogpostModel.get_one_blogpost(blogpost_id)
+  if not post:
+    return custom_response({'error': 'post not found'}, 404)
+  data = blogpost_schema.dump(post).data
+  return custom_response(data, 200)
+
+
+# app initiliazation
+#####################
+# existing code remain #
+######################
