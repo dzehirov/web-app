b77a19322a588d3287ba622b693d4519aad72024
dzehirov <dvz2@njit.edu>
2019-12-18 18:16:12 -0500

    Task: Updated code in app.py

--- blog_api/src/app.py
+++ blog_api/src/app.py
@@ -53,3 +53,36 @@ def create_app(env_name):
     ######################
 
     return app
+
+
+# src/app.py
+
+from flask import Flask
+
+from .config import app_config
+from .models import db, bcrypt
+
+# import user_api blueprint
+from .views.UserView import user_api as user_blueprint  # add this line
+
+
+def create_app(env_name):
+    """
+    Create app
+    """
+
+    # app initiliazation
+    #####################
+    # existing code remain #
+    ######################
+
+    app.register_blueprint(user_blueprint, url_prefix='/api/v1/users')  # add this line
+
+    @app.route('/', methods=['GET'])
+    def index():
+        """
+        example endpoint
+        """
+        return 'Congratulations! Your first endpoint is working'
+
+    return app
