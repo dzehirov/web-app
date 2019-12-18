01a75b71fb628eddd00c6fad37b34fd10e8b1b96
dzehirov <dvz2@njit.edu>
2019-12-18 18:15:05 -0500

    Task: Updated code in UserView.py

--- blog_api/src/views/UserView.py
+++ blog_api/src/views/UserView.py
@@ -97,3 +97,52 @@ def login():
     #####################
     # existing code remain #
     ######################
+
+
+# /src/views/UserView
+
+from flask import request, json, Response, Blueprint
+from ..models.UserModel import UserModel, UserSchema
+from ..shared.Authentication import Auth
+
+user_api = Blueprint('user_api', __name__)
+user_schema = UserSchema()
+
+
+@user_api.route('/', methods=['POST'])
+def create():
+    """
+    Create User Function
+    """
+    #####################
+    # existing code remain #
+    ######################
+    return custom_response({'jwt_token': token}, 201)
+
+
+# add this new method
+@user_api.route('/', methods=['GET'])
+@Auth.auth_required
+def get_all():
+    users = UserModel.get_all_users()
+    ser_users = user_schema.dump(users, many=True).data
+    return custom_response(ser_users, 200)
+
+
+@user_api.route('/login', methods=['POST'])
+def login():
+    #####################
+    # existing code remain #
+    ######################
+    return custom_response({'jwt_token': token}, 200)
+
+
+def custom_response(res, status_code):
+    """
+    Custom Response Function
+    """
+    return Response(
+        mimetype="application/json",
+        response=json.dumps(res),
+        status=status_code
+    )
